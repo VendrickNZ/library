0ad467384741df38fcbf5a31abeec4f49f9ad066
VendrickNZ <jakibisherwood88@gmail.com>
2022-11-22 18:38:14 +1300

    made the modal ui

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const cardContainer = document.querySelector(".card-container");
-const addNewBook = document.getElementById('#add-book');
+const addNewBook = document.getElementById('add-book');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+const bookReadBtn = document.getElementById('book-read');
 
-addNewBook.addEventListener("click", () => {
-    createCard();
+bookReadBtn.addEventListener('click', () => {
+    bookReadBtn.style.backgroundColor = '#e0163b';
+    bookReadBtn.textContent = "Not read"
 })
 
-let myLibrary = [];
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
+const thinkingFastAndSlow = new Book("Thinking, Fast and Slow", "Daniel Kahneman", 300, true);
+const toKillAMockingbird = new Book("To Kill a Mockingbird", "Harper Lee", 281, false);
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
 
 function createCard() {
     const cardBody = document.createElement('div');
@@ -35,9 +53,27 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
-const thinkingFastAndSlow = new Book("Thinking, Fast and Slow", "Daniel Kahneman", 300, true);
-const toKillAMockingbird = new Book("To Kill a Mockingbird", "Harper Lee", 281, false);
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+    }
+});
+
+openModalBtn.addEventListener("click", openModal);
+overlay.addEventListener("click", closeModal);
+closeModalBtn.addEventListener("click", closeModal);
+
+
+addNewBook.addEventListener("click", () => {
+    createCard();
+})
+
+let myLibrary = [];
+
+
+
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(thinkingFastAndSlow);
@@ -46,5 +82,3 @@ addBookToLibrary(toKillAMockingbird);
 for (let i = 0; i < myLibrary.length; i++) {
     console.log(myLibrary[i].title);
 }
-
-
